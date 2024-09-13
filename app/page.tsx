e60ac3720959888b9c0c0a641cc37221c174c127
Yael Borger <yborger217@gmail.com>
2024-09-13 01:38:42 -0400

    Last update fixed the issue - JSON removed and re-added fixed it

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-    return <h1 className="text-3xl font-bold underline">Hmm, Next.js!</h1>
+    return <h1 className="text-3xl font-bold underline">wait it works, Next.js!</h1>
   }
