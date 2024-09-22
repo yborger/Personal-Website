eeb0563da880275ba3c0eb528b5e40b5dae56807
Yael Borger <yborger217@gmail.com>
2024-09-21 21:06:52 -0400

    clearing out reference-work, learning concepts

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
 export default function Page() {
-    return <h1 className="text-3xl font-bold underline">wait it works, Next.js!</h1>
-  }
+  return(
+    <div>
+      <h1 className="text-3xl font-bold underline">wowo fast it it works, Next.js!</h1>
+      <div className="flex flex-col">
+        <div>1</div>
+        <div>1</div>
+        <div>1</div>
+      </div>
+    
+    </div>
+  )
+}
