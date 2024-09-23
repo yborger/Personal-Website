a9c714cf9188ad130dd0cb663e7d0210df2446f3
Yael Borger <yborger217@gmail.com>
2024-09-22 23:38:53 -0400

    Learning day, not too much added content-wise

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
 export default function Page() {
   return(
     <div>
-      <h1 className="text-3xl font-bold underline">wowo fast it it works, Next.js!</h1>
-      <div className="flex flex-col">
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
+      <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-bold underline">wowo fast it it works, Next.js!</h1>
+        <div className="flex flex-col">
+          <div className="p-3 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">1</div>
+          <div>1</div>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded=xl">
+          <slot/>
+          </button>
+          <div>1</div>
+        </div>
+      
+      
+      
+      
+      
       </div>
-    
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,23 @@ import type { Config } from 'tailwindcss'
  
 const config: Config = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,jsx,tsx}', // Note the addition of the `app` directory.
     './components/**/*.{js,ts,jsx,tsx,mdx}',
  
     // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    container:{
+      center: true,
+    },
   },
   plugins: [],
 }
