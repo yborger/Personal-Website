f874e1bfd2082b66bc7f08c109cfb31204ecf45e
Yael Borger <100532471+yborger@users.noreply.github.com>
2025-03-05 04:57:25 +0000

    Nav bar savepoint

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -8,7 +8,7 @@ const navItems = {
     name: 'about',
   }
   ,'/blog': { //will become portfolio section
-    name: 'blog',
+    name: 'portfolio',
   },
   'https://vercel.com/templates/next.js/portfolio-starter-kit': {
     name: '[template link]', //will be moved elsewhere
@@ -18,18 +18,21 @@ const navItems = {
 export function Navbar() {
   return (
     <aside className="-ml-[8px] mb-16 tracking-tight">
-      <div className="bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-400 ... rounded-lg lg:sticky lg:top-20">
+      <div className="bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-400 ... rounded-full lg:sticky lg:top-12">
+        
         <nav
           className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
-        >
-          <div className="flex flex-row space-x-0 pr-10">
+        >          
+        <img src="/ybLogo.png" alt="Logo" className="h-10 w-auto hover:animate-spin" />
+
+          <div className="flex flex-row space-x-0 pr-0">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                  className="transition-all hover:text-neutral-100 dark:hover:text-neutral-400 flex items-center relative py-1 px-2 m-1"
                 >
                   {name}
                 </Link>
