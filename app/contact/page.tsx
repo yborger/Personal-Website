d1b5ba837f843a99ac31b93bb5062228dda26d4e
Yael Borger <100532471+yborger@users.noreply.github.com>
2025-04-09 23:38:09 +0000

    Savepoint - tested fonts, they are not working

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,6 +7,13 @@ export default function Page() {
       </h1>
       
       <div className="flex flex-col md:flex-row mx-auto">
+        
+        <div className="w-full md:w-2/3 p-4">
+          <p className="mb-4">{`I'd love to hear from you!`}</p>
+          <p>{'Email: yaelnborger@gmail.com'}</p>
+          <p>{'LinkedIn: '}<a href="https://linkedin.com/in/yael-borger" target="_blank" rel="noopener noreferrer">linkedin.com/in/yael-borger</a></p>
+          <p>{'GitHub: '}<a href="https://github.com/yborger" target="_blank" rel="noopener noreferrer">github.com/yborger</a></p>        
+        </div>
         <div className="w-full h-full md:w-full p-4 flex "> {/*md:w-1/3 but iframe is weird?*/}
           <div className="flex-row justify-items-center">
             <iframe 
@@ -19,12 +26,6 @@ export default function Page() {
               className="text-blue-500 hover:underline ">{'Download my resume'}
             </a>
           </div>
-        </div>
-        <div className="w-full md:w-2/3 p-4">
-          <p className="mb-4">{`I'd love to hear from you!`}</p>
-          <p>{'Email: yaelnborger@gmail.com'}</p>
-          <p>{'LinkedIn: '}<a href="https://linkedin.com/in/yael-borger" target="_blank" rel="noopener noreferrer">linkedin.com/in/yael-borger</a></p>
-          <p>{'GitHub: '}<a href="https://github.com/yborger" target="_blank" rel="noopener noreferrer">github.com/yborger</a></p>        
         </div>
       </div>  
 
--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,11 @@ export default function Page() {
   return (
     <section>
       <h1 className="ml-4 mb-8 text-2xl font-semibold tracking-tighter">
-        HOME
+        home
       </h1>
-      <p className="mb-4">
-      {'TEXT TEXT'}
+      <p className="ml-4 mb-4 ">
+      {'custom fonts will not be employed until it starts working lol'}
       </p>
-
-      
     </section>
   )
 }
