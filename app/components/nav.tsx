bc2e80756532a3ca290cb9bf7f1a76d36c2c2a89
Yael Borger <100532471+yborger@users.noreply.github.com>
2025-07-14 22:45:55 +0000

    adding nav animation, learning animation

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -19,21 +19,21 @@ const navItems = {
 export function Navbar() {
   return (
     <aside className="-ml-[8px] mb-16 tracking-tight">
-      <div className="bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-400 ... rounded-full lg:sticky lg:top-12">
+      <div className="w-full bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-400 ... rounded-full lg:sticky lg:top-12">
         
         <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="container flex flex-row items-start relative md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >          
-        <img src="/ybLogo.png" alt="Logo" className="h-10 w-auto hover:animate-spin" />
+          <img src="/ybLogo.png" alt="logo" className="logo h-16 w-auto" />
 
-          <div className="flex flex-row space-x-0 pr-0">
+          <div className="navMenu flex flex-row py-4 space-x-0 pr-0">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-100 dark:hover:text-neutral-400 flex items-center relative py-1 px-2 m-1"
+                  className="transition-all hover:text-neutral-100 dark:hover:text-neutral-400 flex relative px-2 m-1"
                 >
                   {name}
                 </Link>
