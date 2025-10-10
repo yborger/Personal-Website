2642aff6fee956cac615dac4c734f68b4b01711a
Yael Borger <100532471+yborger@users.noreply.github.com>
2025-10-10 19:58:33 +0000

    pause - phasecards need realignment

--- app/components/PhaseCard.tsx
+++ app/components/PhaseCard.tsx
@@ -25,11 +25,16 @@ interface CardProps {
 
 export default function PhaseCard({title, description, image, bg}: CardProps){
     return(
-        <div className={`border-4 border-black rounded-md gap-4 m-8 flex items-center justify-center bg-gradient-to-b ${bg}`}>
-                <img src={image} alt={title} className= "w-1/2 h-auto rounded-xl m-8" />
+        <div className={`border-4 border-black rounded-md gap-4 m-8 shadow-lg p-8 w-3/4 max-w-4xl bg-gradient-to-b ${bg}`}>
+                <h2 className="text-2xl font-bold mb-6 text-left">{title}</h2>
+                <div className="flex-1 flex justify-center">
+                        <img 
+                                src={image} 
+                                alt={title} 
+                                className= "w-1/2 h-auto object-contain rounded-xl m-4" />
+                </div>
                 <div className= "w=1/2 m-8">
-                        <h2 className="text-2xl font-bold">{title}</h2>
-                        <p className="text-m">{description}</p>
+                <p className="flex-1 text-gray-700 text-lg text-left">{description}</p>
                 </div>
         </div>
     )
--- app/portfolio/cases/example/page.tsx
+++ app/portfolio/cases/example/page.tsx
@@ -1,6 +1,8 @@
 
 import { notFound } from 'next/navigation'
 import { CustomMDX } from 'app/components/mdx'
+import {motion} from "motion/react"
+import PhaseCard from 'app/components/PhaseCard'
 
 export const metadata = {
   "title": "EXAMPLE Title",
@@ -23,8 +25,19 @@ export default function Page() {
         {metadata.title}
       </h1>
       <div className="flex justify-center items-center mt-2 mb-8 text-sm">
-
-
+        
+    <PhaseCard
+        title="Phase 1: Design"
+        description="In this phase, we focused on user experience and interface design, ensuring that the product is both functional and visually appealing."
+        image="/concept artwork/bee-leaf/bee-v3.jpg"
+        bg="from-purple-400 via-pink-500 to-red-500"
+    />  
+    <PhaseCard 
+        title="Phase 2: Development"
+        description="This phase involved coding the core functionalities, integrating APIs, and ensuring the application is robust and scalable."
+        image="/concept artwork/orange/group_hug-v1.png"
+        bg="from-green-400 via-blue-500 to-purple-500"
+    />
       </div>
     </section>
   )
