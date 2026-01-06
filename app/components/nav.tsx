f80bc4fd954d94dffe27b571afaffbb30cfc1612
Yael Borger <100532471+yborger@users.noreply.github.com>
2026-01-06 04:02:23 +0000

    dark/light switch exists in navbar, flips as desired, does not currently shift theme

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -38,29 +38,33 @@ export function Navbar() {
                   {name}
                 </Link>
               )
-            })}
-            <button className="lightDark cursor-pointer transition-all hover:text-neutral-100 dark:hover:text-neutral-400 flex relative px-2 m-1 ">☀︎☾</button>
-            
-<label className="inline-flex items-center cursor-pointer">
-  <input type="checkbox" value="" className="sr-only peer"/>
-
-      
-  <div className="relative w-9 h-5 bg-neutral-quaternary  peer-focus:ring-4 peer-focus:ring-brand-soft dark:peer-focus:ring-brand-soft rounded-full
-  peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-buffer after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-brand"></div>
-  <span className="select-none ms-3 text-sm font-medium text-heading"></span>
-</label>
-<label className="flex items-center cursor-pointer select-none text-dark dark:text-white group">
+            })}            
+          </div>
+  {/* the light/dark switch */}
+    <label className="lightdark py-4 m-1.5 mr-5 ml-auto cursor-pointer select-none text-dark dark:text-white group">
       <div className="relative">
-        <input id="2" type="checkbox" className="peer sr-only" />
-        <div className="h-5 rounded-full shadow-inner bg-gray-3 dark:bg-dark-2 w-14"></div>
+        <input type="checkbox" className="peer sr-only" />
+        {/* the track -- it's 20% transparent white for glass effect*/}
+        <div className="h-5 rounded-full w-14 bg-white/20 shadow-lg ring-1 ring-black/5"></div>
         <div
           className="absolute left-0 transition bg-white rounded-full dot shadow-switch-1 dark:bg-dark-4 -top-1 h-7 w-7 group-has-checked:translate-x-full group-has-checked:bg-primary">
+    {/* moon */}
+          <span className="hidden group-has-checked:inline">
+              <svg className="h-6 w-6 text-gray-500" fill="none" viewBox="-2 -1 24 24" stroke="currentColor"> 
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"/>
+              </svg>
+          </span>
+    {/* sun */}
+          <span className="visible group-has-checked:hidden">
+              <svg className="h-6 w-6 text-gray-500" fill="none" viewBox="-1.5 -2 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+              </svg>
+          </span>
         </div>
       </div>
     </label>
 
-            
-          </div>
+           
         </nav>
       </div>
     </aside>
