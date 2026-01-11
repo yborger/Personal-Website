1b6ffcdfeaff77f497be369aa17381736a7ccbb8
Yael Borger <100532471+yborger@users.noreply.github.com>
2026-01-11 03:04:14 +0000

    comment out the last dark/light attempt, pixel-adjust the sun and moon

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Link from 'next/link'
-import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
+//import { useTheme } from 'next-themes';
+//import { useEffect, useState } from 'react';
 
 const navItems = {
   '/': {
@@ -20,16 +20,16 @@ const navItems = {
 }
 
 export function Navbar() {
-  const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
+  //const { theme, setTheme } = useTheme();
+  //const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+  //useEffect(() => {
+  //  setMounted(true);
+  //}, []);
 
-  if (!mounted) {
-    return null;
-  }
+  //if (!mounted) {
+  //  return null;
+  //}
 
   return (
     <aside className="-ml-[8px] mb-16 tracking-tight">
@@ -58,8 +58,8 @@ export function Navbar() {
     <label className="lightdark py-4 m-1.5 mr-5 ml-auto cursor-pointer select-none text-dark  group">
       <div className="relative">
         <input type="checkbox" className="peer sr-only" 
-          checked={theme === 'dark'}
-          onChange={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+          //checked={theme === 'dark'}
+          //onChange={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
         />
         {/* the track -- it's 20% transparent white for glass effect*/}
         <div className="h-5 rounded-full w-14 bg-white/20 shadow-lg ring-1 ring-black/5"></div>
@@ -67,13 +67,13 @@ export function Navbar() {
           className="absolute left-0 transition bg-white rounded-full dot shadow-switch-1 -top-1 h-7 w-7 group-has-checked:translate-x-full group-has-checked:bg-primary">
     {/* moon */}
           <span className="hidden group-has-checked:inline">
-              <svg className="h-6 w-6 text-gray-500" fill="none" viewBox="-2 -1 24 24" stroke="currentColor"> 
+              <svg className="h-6 w-6 text-gray-500" fill="none" viewBox="-2.5 -2 24 24" stroke="currentColor"> 
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"/>
               </svg>
           </span>
     {/* sun */}
           <span className="visible group-has-checked:hidden">
-              <svg className="h-6 w-6 text-gray-500" fill="none" viewBox="-1.5 -2 24 24" stroke="currentColor">
+              <svg className="h-6 w-6 text-gray-500" fill="none" viewBox="-2 -2 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
               </svg>
           </span>
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,4 +1,3 @@
-// src/app/providers.tsx
 'use client'
 
 import { ThemeProvider } from 'next-themes'
