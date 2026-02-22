0b0439a1962ef9437cb03a208308149ce93a55ca
Yael Borger <100532471+yborger@users.noreply.github.com>
2026-02-22 02:59:39 +0000

    Pause in implementation - phase cards mostly complete

--- app/components/PhaseCard.tsx
+++ app/components/PhaseCard.tsx
@@ -1,41 +1,60 @@
+"use client"
 
-/*
+import {
+  motion,
+  useScroll,
+  useTransform,
+  useSpring,
+  MotionValue,
+} from "motion/react"
+import { useRef } from "react"
 
-    Notes/checklist:
-
-    This is specifically for the "scroll and it will appear" section of the projects
-    So these are the cards that fan out/in as you scroll
-
-    -title: at the top, say the PHASE TITLE (design phase, planning, etc.)
-    -img: image with image carousel option?
-            -can i make the img parameter be like a folder route and have it display all images in the folder in a carousel?
-    -descrip: the writing that goes along with this card
-            -expect it to be between 2-5 sentences
-    
-    Animation into global.css, left appear is written so now we need the full card
-
- */
 interface CardProps {
+        number: number;
         title: string;
         description: string;
         image: string;
         bg: string;
 }
 
-//not the final look, just a placeholder for now
 
-export default function PhaseCard({title, description, image, bg}: CardProps){
+export default function PhaseCard({number, title, description, image, bg}: CardProps){
+    const ref = useRef(null)
+    const { scrollYProgress } = useScroll({ 
+        target: ref,
+        offset: ["start end", "end start"],
+ })
+    const y = useTransform(scrollYProgress, [0,1], [100, -100])
+
     return(
-        <div className={`rounded-md p-6 m-4 shadow-lg w-auto max-w-4xl bg-gradient-to-b ${bg}`}>
+        <section 
+                ref={ref}
+                className="h-3/4 flex items center justify-center relative">
+        {/* PHASE NUMBER - PARALLAX SETUP */}
+        <motion.div 
+                style={{y}}
+                className="absolute right-16 text-7xl font-bold opacity-20">
+                {number}
+        </motion.div>
+
+        {/* CONTENT */}
+        <motion.div
+                initial={{opacity:0,y:60}}
+                whileInView={{opacity:1,y:0}}
+                transition={{duration:0.6, ease:"easeInOut"}}
+                viewport={{once:true}}
+                className={'rounded-md p-6 m-4 shadow-lg max-w-4xl bg-gradient-to-b ' + bg}>
+
                 <h2 className="text-2xl font-bold mb-6 text-left">{title}</h2>
-                <div className="m-4 flex flex-col items-start justify-center gap-4">
+                <div className="m-4 flex flex-row items-start justify-center gap-4">
                         <img 
                                 src={image} 
                                 alt={title} 
-                                className= "w-auto h-auto rounded-xl" />
+                                className= "w-3/5 h-auto rounded-xl" />
                 
                         <p className="w-auto text-lg text-left">{description}</p>
                 </div>
-        </div>
+        </motion.div>
+        </section>
     )
 }
