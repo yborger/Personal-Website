b12c0dc32f37982b95a81747f2996c1dbef5b3c5
Yael Borger <100532471+yborger@users.noreply.github.com>
2026-03-07 04:48:23 +0000

    Snap implemented, phasecards changed over to grid w/ mobile adjust

--- app/components/PhaseCard.tsx
+++ app/components/PhaseCard.tsx
@@ -7,21 +7,24 @@ interface CardProps {
         bg: string;
 }
 
- 
-export default function PhaseCard({number, title, description, image, bg}: CardProps){
+ export default function PhaseCard({number, title, description, image, bg}: CardProps){
     return(
-        <section className={"h-3/4 flex items-center justify-center relative"}>
-        <div className={"rounded-md p-6 m-4 shadow-lg max-w-4xl bg-gradient-to-b " + bg}>
+        <section className="h-3/4 flex items-center justify-center relative">
+            <div className={"rounded-md p-6 m-4 shadow-lg max-w-4xl bg-gradient-to-b " + bg}>
                 <h2 className="text-2xl font-bold mb-6 text-left">{title}</h2>
-                <div className="m-4 flex flex-row items-start justify-center gap-4">
-                        <img 
-                                src={image} 
-                                alt={title} 
-                                className= "w-3/5 h-auto rounded-xl" />
-                
-                        <p className="w-auto text-lg text-left">{description}</p>
+
+                <div className="m-4 grid grid-cols md:grid-cols-[3fr_2fr] items-start gap-4">
+                    <img 
+                        src={image} 
+                        alt={title} 
+                        className="w-3/5 h-auto rounded-xl justify-self-center"
+                    />
+
+                    <p className="text-lg text-left">
+                        {description}
+                    </p>
                 </div>
-        </div>
+            </div>
         </section>
     )
 }
